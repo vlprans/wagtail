fbcab398e81a53db3bd8393754e65c75426f5c82
Matt Westcott <matt@west.co.tt>
2014-06-02 23:30:00 +0100

    Implement before_serve_page hook

--- wagtail/tests/wagtail_hooks.py
+++ wagtail/tests/wagtail_hooks.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+
 from wagtail.wagtailcore import hooks
 
 def editor_css():
@@ -8,3 +10,9 @@ hooks.register('insert_editor_css', editor_css)
 def editor_js():
     return """<script src="/path/to/my/custom.js"></script>"""
 hooks.register('insert_editor_js', editor_js)
+
+
+def block_googlebot(page, request):
+    if request.META.get('HTTP_USER_AGENT') == 'GoogleBot':
+        return HttpResponse("<h1>bad googlebot no cookie</h1>")
+hooks.register('before_serve_page', block_googlebot)
